312201c4d03b2ba012aeea079262b42d014b4de9
rizalaji1st <rizalaji561@gmail.com>
2021-08-20 19:38:10 +0700

    Mempersiapkan frontend dengan import bootstrap dan membuat component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment} from "react";
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Movies from './components/Movies';
+import Admin from './components/Admin';
+import Home from './components/Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt3">
+            Go Watch a Movie!
+          </h1>
+        </div>
+          <hr className="mb-3"/>
+          <div className="row">
+            <div className="col-md-2">
+                <nav>
+                  <ul className="list-group">
+                    <li className="list-group-item">
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li className="list-group-item">
+                      <Link to="/movies">Movies</Link>
+                    </li>
+                    <li className="list-group-item">
+                      <Link to="/admin">Manage Catalogue</Link>
+                    </li>
+                  </ul>
+                </nav>
+            </div>
+            <div className="col-md-10">
+              <Switch>
+                <Route path="/movies">
+                  <Movies />
+                </Route>
+                <Route path="/admin">
+                  <Admin />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+      </div>
+    </Router>
   );
 }
-
-export default App;
