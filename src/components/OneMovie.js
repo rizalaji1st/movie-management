31af0ea426399cd47ccc8bdd98e1a58ff057da99
rizalaji1st <rizalaji561@gmail.com>
2021-08-22 20:56:24 +0700

    setup untuk retrieve satu movie

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -1,36 +1,96 @@
 import React, { Component, Fragment } from 'react'
 
 export default class OneMovie extends Component{
-    state = {movie:{}};
+    state = {
+                movie:{},
+                isLoaded: false,
+                error: null
+            };
 
     componentDidMount(){
-        this.setState({movie:{
-            id: this.props.match.params.id,
-            title: "Some Movie",
-            runtime: 150
-        }})
+        fetch("http://localhost:4000/v1/movie/"+this.props.match.params.id)
+            // .then((response)=> response.json())
+            .then((response) => {
+                if(response.status !== "200"){
+                    let err = Error;
+                    err.message = "Invalid response code : "+response.status;
+                    this.setState({error: err});
+                }
+                return response.json();
+            })
+            .then((json) =>{
+                this.setState({
+                    movie: json.movie,
+                    isLoaded: true,
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+                )
+            })
     }
 
     render(){
-        return(
-            <Fragment>
-                <h2>Movie: {this.state.movie.title}</h2>
-                <table className="table table-compact table-striped">
-                    <thead>
+        const {movie, isLoaded, error} = this.state;
+        
+        if(movie.genres){
+            movie.genres = Object.values(movie.genres);
+        }
+        else{
+            movie.genres = [];
+        }
 
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><strong>Title : </strong></td>
-                            <td>{this.state.movie.title} {this.state.movie.id}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Run Time : </strong></td>
-                            <td>{this.state.movie.runtime}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </Fragment>
-        );
+        if (error){
+            return <div>Error : {error.message}</div>
+        }
+        else if(!isLoaded){
+            return<p>Loading...</p>
+        }
+        else{
+            return(
+                <Fragment>
+                    <h2>
+                        Movie: {movie.title} ({movie.year})
+                    </h2>
+
+                    <div className="float-left">
+                        <small>{movie.mpaa_rating}</small>
+                    </div>
+                    <div className="float-right">
+                        {movie.genres.map((m,index) => (
+                            <span className="badge badge-secondary mr-1" key={index}>
+                                {m}
+                            </span>
+                        ))}
+                    </div>
+                    <div className="clearfix"></div>
+
+                    <hr />
+
+                    <table className="table table-compact table-striped">
+                        <thead>
+    
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>Title : </strong></td>
+                                <td>{movie.title} {this.state.movie.id}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Description : </strong></td>
+                                <td>{movie.description}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Run Time : </strong></td>
+                                <td>{movie.runtime}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Fragment>
+            );
+        }
     }
 }
