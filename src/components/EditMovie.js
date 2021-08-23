ab6244aacc6617911d08458bbc11b9de27068887
rizalaji1st <rizalaji561@gmail.com>
2021-08-23 21:08:16 +0700

    bind form ke state

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -8,51 +8,100 @@ export default class EditMovie extends Component{
         error: null,
     }
 
+    constructor(props){
+        super(props);
+        this.state = {
+            movie: {
+                id: 0,
+                title: "",
+                release_date: "",
+                runtime: "",
+                mpaa_rating: "",
+                rating: "",
+                description: "",
+            },
+            isLoaded : false,
+            error: null,
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = (evt) =>{
+        console.log("Form submitted");
+        evt.preventDefault();
+    }
+
+    handleChange = (evt) => {
+        let value = evt.target.value;
+        let name = evt.target.name;
+        this.setState((prevState) => ({
+            movie: {
+                ...prevState.movie,
+                [name]: value,
+            }
+        }))
+    }
+
     render(){
         let {movie} = this.state;
         return(
             <Fragment>
                 <h2>Add/Edit Movie</h2>
                 <hr />
-                <form method="post">
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="hidden"
+                        name="id"
+                        id="id"
+                        value={movie.id}
+                        onChange={this.handleChange}
+                    />
                     <div className="mb-3">
-                        <label for="title" className="form-label">
+                        <label htmlFor="title" className="form-label">
                             Title
                         </label>
                         <input type="text" 
                             className="form-control" 
                             id="title" 
                             name="title"
-                            value={movie.title}/>
+                            value={movie.title}
+                            onChange={this.handleChange}
+                            />
                     </div>
 
                     <div className="mb-3">
-                        <label for="release_date" className="form-label">
+                        <label htmlFor="release_date" className="form-label">
                             Release Date
                         </label>
                         <input type="text" 
                             className="form-control" 
                             id="release_date" 
                             name="release_date"
-                            value={movie.release_date}/>
+                            value={movie.release_date}
+                            onChange={this.handleChange}
+                            />
                     </div>
 
                     <div className="mb-3">
-                        <label for="runtime" className="form-label">
+                        <label htmlFor="runtime" className="form-label">
                             Runtime
                         </label>
                         <input type="text" 
                             className="form-control" 
                             id="runtime" 
                             name="runtime"
-                            value={movie.runtime}/>
+                            value={movie.runtime}
+                            onChange={this.handleChange}
+                            />
                     </div>
 
                     <div className="mb-3">
-                        <label for="mpaa_rating" className="form-label">
+                        <label htmlFor="mpaa_rating" className="form-label">
                             MPAA Rating
                         </label>
-                        <select className="form-control" value={movie.mpaa_rating}>
+                        <select className="form-control" name="mpaa_rating" value={movie.mpaa_rating} onChange={this.handleChange}>
                             <option>Choose...</option>
                             <option value="G">G</option>
                             <option value="PG">PG</option>
@@ -63,27 +112,38 @@ export default class EditMovie extends Component{
                     </div>
 
                     <div className="mb-3">
-                        <label for="rating" className="form-label">
+                        <label htmlFor="rating" className="form-label">
                             Rating
                         </label>
                         <input type="text" 
                             className="form-control" 
                             id="rating" 
                             name="rating"
-                            value={movie.rating}/>
+                            value={movie.rating}
+                            onChange={this.handleChange}
+                            />
                     </div>
 
                     <div className="mb-3">
-                        <label for="description" className="form-label">
+                        <label htmlFor="description" className="form-label">
                             Description
                         </label>
-                        <textarea className="form-control" id="description" name="description" rows="3">
-                            {movie.description}
-                        </textarea>
+                        <textarea 
+                            className="form-control" 
+                            id="description" 
+                            name="description" 
+                            rows="3" 
+                            onChange={this.handleChange}
+                            value={movie.value}
+                            />
                         <hr/>
                         <button className="btn btn-primary">Save </button>
                     </div>
                 </form>
+
+                <div className="mt-3">
+                    <pre>{JSON.stringify(this.state, null, 3)}</pre>
+                </div>
             </Fragment>
         )
     }
