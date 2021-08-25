5c290073ffe85403a19a65a2358eab346e992e1e
rizalaji1st <rizalaji561@gmail.com>
2021-08-25 19:24:39 +0700

    Menambahkan alert dan delete movie

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -4,6 +4,9 @@ import Input from'./form-components/Input'
 import TextArea from './form-components/TextArea'
 import Select from './form-components/Select'
 import Alert from './ui-components/Alert'
+import { Link } from 'react-router-dom'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 export default class EditMovie extends Component{
 
@@ -143,6 +146,42 @@ export default class EditMovie extends Component{
         }
     }
 
+    confirmDelete = (e) => {
+
+        confirmAlert({
+            title: 'Delete Movie ?',
+            message: 'Are you sure?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => {
+                        fetch("http://localhost:4000/v1/admin/deletemovie/"+this.state.movie.id, {method: "GET"})
+                            .then(response => response.json)
+                            .then(data => {
+                                if(data.error){
+                                    this.setState({
+                                        alert: {type: "alert-danger", message: data.error.message}
+                                    })
+                                }else{
+                                    // this.setState({
+                                    //     alert: {type: "alert-success", message: "Movie deleted"}
+                                    // })
+
+                                    this.props.history.push({
+                                        pathname: "/admin",
+                                    })
+                                }
+                            })
+                    }
+                },
+                {
+                    label: 'No',
+                    onClick: () => {}
+                }
+            ]
+        });
+    }
+
     render(){
         let {movie, isLoaded, error} = this.state;
         
@@ -223,6 +262,15 @@ export default class EditMovie extends Component{
                         />
                         <hr/>
                         <button className="btn btn-primary">Save </button>
+                        <Link to="/admin" className="btn btn-warning ml-1">
+                            Cancel
+                        </Link>
+                        {movie.id > 0 && (
+                            <a href="#!" onClick={() => this.confirmDelete()}
+                                className="btn btn-danger ml-1">
+                                    Delete
+                                </a>
+                        )}
                     </form>
     
                     {/* <div className="mt-3">
